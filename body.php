491317f5c4a32780d4545c21d351103c1f549726
Erlendbro <erlendbro17@hotmail.com>
2020-05-27 05:58:08 +0200

    Final Backup

--- body.php
+++ body.php
@@ -6,38 +6,35 @@
 
 
     <!--Flexbox item 01-->
+    <a href="games.php">
     <li class="flex-item">
         <h2 class="mobile-lables">Games</h2>
-
-        <a href="games.php">
             <div class="container">
                 <img src="img/Exiles01.png" alt="Art" class="image">
                 <div class="overlay">
                     <div class="text">GAMES</div>
                 </div>
             </div>
-        </a>
     </li>
+        </a>
 
     <!--Flexbox item 02-->
+    <a href="personal.php">
     <li class="flex-item">
         <h2 class="mobile-lables">Personal Projects</h2>
-
-        <a href="personal.php">
             <div class="container">
-                <img src="img/mountain01.png" alt="Art" class="image">
+                <img src="img/Mountain01.png" alt="Art" class="image">
                 <div class="overlay">
                     <div class="text">PERSONAL PROJECTS</div>
                 </div>
             </div>
-        </a>
     </li>
+        </a>
 
     <!--Flexbox item 03-->
+    <a href="3D.php">
     <li class="flex-item">
         <h2 class="mobile-lables">3D Modeling</h2>
-
-        <a href="3D.php">
 
             <div class="container">
                 <img src="img/VendingMachine01.png" alt="Art" class="image">
@@ -45,15 +42,14 @@
                     <div class="text">3D MODELING</div>
                 </div>
             </div>
-        </a>
     </li>
+        </a>
 
 
     <!--Flexbox item 04-->
+    <a href="programming.php">
     <li class="flex-item">
         <h2 class="mobile-lables">Programming</h2>
-
-        <a href="programming.php">
 
             <div class="container">
                 <img src="img/Programming.png" alt="Art" class="image">
@@ -61,6 +57,6 @@
                     <div class="text">PROGRAMMING</div>
                 </div>
             </div>
-        </a>
     </li>
+        </a>
 </ul>
