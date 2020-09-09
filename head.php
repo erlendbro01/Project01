f9341a08cf5f259c8232e9bf2912ef682bf29dac
Erlendbro <erlendbro17@hotmail.com>
2020-09-09 14:38:37 +0200

    ye

--- head.php
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" type="text/css" href="stylesheet.css?v=0.0.2"/>
-
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
-<script src="jquery-3.5.1.min.js"></script>
-<title>Erlend Brodal</title>
-
